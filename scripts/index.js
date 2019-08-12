5fb9afd878ce7e1fc8c8b8fd376e7a6507f44db6
Yujie0830 <yujie0830@gmail.com>
2019-08-12 13:54:22 +0800

    yujie

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,32 @@
 /* JS Document */
 
+// NAV
+$(document).ready(function(){
+	
+	// browser window scroll (in pixels) after which the "back to top" link is shown
+	var adj_h=10;// 修正畫面與滾輪之前的差異pixel數
+		offset = $("#about-block").offset().top - adj_h,
+		//browser window scroll (in pixels) after which the "back to top" link opacity is reduced
+		offset_opacity = $("#about-block").offset().top - adj_h,
+		//duration of the top scrolling animation (in ms)
+		scroll_top_duration = 700,
+		//grab the "back to top" link
+		$nav = $('.nav');
+
+	//hide or show the "back to top" link
+	$(window).scroll(function(){
+		( $(this).scrollTop() > offset ) ? $nav.addClass('nav-is-visible') : $nav.removeClass('nav-is-visible nav-fade-out');
+		if( $(this).scrollTop() > offset_opacity ) { 
+			$nav.addClass('cd-fade-out');
+		}
+	});
+
+	$(".burger").click(function(){
+      $(".burger").toggleClass("open");
+      $(".nav-menu-content").toggleClass("open").addClass("animated fadeInDown");
+    });
+});
+
 $(window).ready(function () {
 	arrow();
 });
@@ -74,10 +101,45 @@ function banner(){
 // 滑到指定位置 -------------------------
 $(function btn(){
 	// 選單 的 click 事件，畫面移至下方活動介紹區塊的滾動效果
-	$('.btn-nextArrow').click(function(){
+	$('.nav>ul>li:nth-child(3) a').click(function(){
 		var $MENU_info = (window.opera) ? (document.compatMode === "CSS1Compat" ? $('html') : $('body')) : $('html,body');
 		$MENU_info.animate({
 			scrollTop: $('#about-block').offset().top
+		}, 400, 'linear'); 
+		return false;
+	});
+	$('.nav>ul>li:nth-child(4) a').click(function(){
+		var $MENU_info = (window.opera) ? (document.compatMode === "CSS1Compat" ? $('html') : $('body')) : $('html,body');
+		$MENU_info.animate({
+			scrollTop: $('#portfolio-block').offset().top
+		}, 400, 'linear'); 
+		return false;
+	});
+	$('.nav>ul>li:nth-child(5) a').click(function(){
+		var $MENU_info = (window.opera) ? (document.compatMode === "CSS1Compat" ? $('html') : $('body')) : $('html,body');
+		$MENU_info.animate({
+			scrollTop: $('#contact-block').offset().top
+		}, 400, 'linear'); 
+		return false;
+	});
+	$('.nav-menu-content>ul>li:nth-child(1) a').click(function(){
+		var $MENU_info = (window.opera) ? (document.compatMode === "CSS1Compat" ? $('html') : $('body')) : $('html,body');
+		$MENU_info.animate({
+			scrollTop: $('#about-block').offset().top
+		}, 400, 'linear'); 
+		return false;
+	});
+	$('.nav-menu-content>ul>li:nth-child(2) a').click(function(){
+		var $MENU_info = (window.opera) ? (document.compatMode === "CSS1Compat" ? $('html') : $('body')) : $('html,body');
+		$MENU_info.animate({
+			scrollTop: $('#portfolio-block').offset().top
+		}, 400, 'linear'); 
+		return false;
+	});
+	$('.nav-menu-content>ul>li:nth-child(3) a').click(function(){
+		var $MENU_info = (window.opera) ? (document.compatMode === "CSS1Compat" ? $('html') : $('body')) : $('html,body');
+		$MENU_info.animate({
+			scrollTop: $('#contact-block').offset().top
 		}, 400, 'linear'); 
 		return false;
 	});
@@ -124,3 +186,5 @@ $(function btn(){
 		return false;
 	});
 });
+
+
